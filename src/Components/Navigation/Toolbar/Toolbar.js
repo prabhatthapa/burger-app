d78220ea2bedc470c12b29cf1f960793f5eebb18
Prabhat Thapa <prabhat4ever@gmail.com>
2021-03-23 23:24:08 +0000

    Add side drawer toggle feature

--- src/Components/Navigation/Toolbar/Toolbar.js
+++ src/Components/Navigation/Toolbar/Toolbar.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
+import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
 
 import "./Toolbar.css";
 
 const toolbar = (props) => (
   <header className="Toolbar">
-    <div>Menu</div>
+    <DrawerToggle clicked={props.drawerToggleClicked} />
     <Logo />
     <nav className="DesktopOnly">
       <NavigationItems />
